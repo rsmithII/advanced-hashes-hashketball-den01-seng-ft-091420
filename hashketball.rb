98c8b10f10ed2bcb410c2d24a0b58f651b299ff7
Rodney Smith II <r_smith3@hotmail.com>
2020-09-06 21:46:28 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
 # Write your code below game_hash
+require 'pry'
 def game_hash
   {
     home: {
@@ -126,4 +127,88 @@ def game_hash
   }
 end
 
-# Write code here
+def get_team_helper(team)
+  case team 
+  when game_hash[:home][:team_name]
+    game_hash[:home]
+  when game_hash[:away][:team_name]
+    game_hash[:away]
+  end
+end
+
+def player_helper
+  game_hash[:home][:players].merge(game_hash[:away][:players])
+end
+
+def num_points_scored(player)
+    game_hash[:home][:players].each do |player_data|
+      if player_data[:player_name] == player
+        return player_data[:points]
+  end
+ end
+ game_hash[:away][:players].each do |player_data|
+      if player_data[:player_name] == player
+        return player_data[:points]
+  end
+ end
+end
+
+def shoe_size(player)
+  game_hash[:away][:players].each do |p|
+    if p[:player_name] == player
+      return p[:shoe]
+  end
+ end
+ game_hash[:home][:players].each do |p|
+    if p[:player_name] == player
+      return p[:shoe]
+    end
+  end
+end
+
+def team_colors(team)
+  if game_hash[:home][:team_name] == team
+    return game_hash[:home][:colors]
+end
+if game_hash[:away][:team_name] == team
+    return game_hash[:away][:colors]
+  end
+ end
+
+def team_names
+ game_hash.map do |key, value|
+   value[:team_name]
+end
+end
+
+def player_numbers(team)
+  get_team_helper(team)[:players].map {|player| player[:number]}
+end
+
+def player_stats(player)
+  game_hash[:away][:players].each do |p|
+    if p[:player_name] == player
+      return p
+  end
+ end
+ game_hash[:home][:players].each do |p|
+    if p[:player_name] == player
+      return p
+    end
+  end
+end
+
+def big_shoe_rebounds
+  biggest_shoe = 0
+  rebounds = 0
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:shoe] > biggest_shoe
+        biggest_shoe = player[:shoe]
+        rebounds = player[:rebounds]
+       # binding.pry
+      end
+    end
+  end
+  return rebounds
+end
